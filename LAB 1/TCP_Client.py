bfe2a06604625f346b3c41dd4e926dff709b7e85
Daanyaal Ahad Saeed <daanyaal.saeed@sjsu.edu>
2018-10-01 00:28:56 -0700

    Add comments to TCP_Server.py

--- LAB 1/TCP_Client.py
+++ LAB 1/TCP_Client.py
@@ -9,23 +9,23 @@ HOST = socket.gethostname()
 # Port to listen on, this must be the same for client and server
 PORT = 12345                            
 
-#Take input from user
+# Take input from user
 num = str(input("Enter a number: "))
 
-#Set up connection type using AF_INET for socekt programming and SOCK_STREAM for a TCP connection
+# Set up connection type using AF_INET for socekt programming and SOCK_STREAM for a TCP connection
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
     
-    #Start connection with server
+    # Start connection with server
     s.connect((HOST, PORT))
     
-    #Send encoded message
+    # Send encoded message
     s.send(num.encode())
     
-    #Receive/Listen to message from Server
+    # Receive/Listen to message from Server
     data = s.recv(1024)
 
-#Display message from server
+# Display message from server
 print('Message from Server:', data.decode())
 
-#Close connection
+# Close connection
 s.close()
--- LAB 1/TCP_Server.py
+++ LAB 1/TCP_Server.py
@@ -1,26 +1,44 @@
 # TCP Protocol Server
 
+# Import socket module
 import socket
 
-HOST = socket.gethostname()         # localhost
-PORT = 12345                        # Port to listen on
+# localhost
+HOST = socket.gethostname()             
 
+# Port to listen on, this must be the same for client and server
+PORT = 12345                            
+
+# Set up connection type using AF_INET for socekt programming and SOCK_STREAM for a TCP connection
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+    # Bind the socket to the address
     s.bind((HOST, PORT))
+
+    # Enable server to accept connections
     s.listen()
+
+    # Accept the connection
     conn, addr = s.accept()
+
+
     with conn:
         print('Got connection from', addr)
         while True:
+            # Receive data from client
             data = conn.recv(1024)
 
+            # If no data is received, end connection
             if not data:
                 break
 
+            # Store data received in num variable
             num = int(data.decode())
 
+            # Print the data received
             print("Message from Client:", num)
 
+            # The following algorithm determins if num is a prime number or not
             if num > 1:
                 for i in range(2,num):
                     if (num % i) == 0:
@@ -31,4 +49,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             else:
                 res = ("%d is not a prime number" %num)
             
+            # Send back result of algorithm
             conn.send(res.encode())
