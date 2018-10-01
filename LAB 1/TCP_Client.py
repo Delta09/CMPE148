5d0e863200a9b6605a3c46a0ac1d8bf6c507f928
Daanyaal Ahad Saeed <daanyaal.saeed@sjsu.edu>
2018-10-01 00:10:47 -0700

    Add comments to TCP_Client.py

--- LAB 1/TCP_Client.py
+++ LAB 1/TCP_Client.py
@@ -1,16 +1,31 @@
 # TCP Protocol Client
 
-import socket                           # Import socket module
+# Import socket module
+import socket
 
-HOST = socket.gethostname()             # localhost
-PORT = 12345                            # Port to listen on
+# localhost
+HOST = socket.gethostname()             
 
+# Port to listen on, this must be the same for client and server
+PORT = 12345                            
+
+#Take input from user
 num = str(input("Enter a number: "))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#Set up connection type using AF_INET for socekt programming and SOCK_STREAM for a TCP connection
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
+    
+    #Start connection with server
     s.connect((HOST, PORT))
+    
+    #Send encoded message
     s.send(num.encode())
+    
+    #Receive/Listen to message from Server
     data = s.recv(1024)
 
+#Display message from server
 print('Message from Server:', data.decode())
+
+#Close connection
 s.close()
