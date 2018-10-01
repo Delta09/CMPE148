ab202a7788c50ff7fabdb95759c89a50c2a9295d
Daanyaal Ahad Saeed <daanyaal.saeed@sjsu.edu>
2018-10-01 00:40:14 -0700

    Add comments to UDP_Server.py

--- LAB 1/UDP_Server.py
+++ LAB 1/UDP_Server.py
@@ -1,20 +1,31 @@
 # UDP Protocol Server
 
+# Import socket module
 import socket
 
-HOST = socket.gethostname()         # localhost
-PORT = 12345                        # Port to listen on
+# localhost
+HOST = socket.gethostname()             
 
+# Port to listen on, this must be the same for client and server
+PORT = 12345           
+
+# Set up connection type using AF_INET for socekt programming and SOCK_STREAM for a TCP connection
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+# Bind the socket to the address
 s.bind((HOST, PORT))
 
 while True:
+    # Receive data from client
     data, addr = s.recvfrom(1024)
+    
+    # Store data received in num variable
     num = int(data.decode())
 
+    # Print the data received
     print("Message from Client:", num)
 
+    # The following algorithm determins if num is a prime number or not
     if num > 1:
                 for i in range(2,num):
                     if (num % i) == 0:
@@ -24,4 +35,6 @@ while True:
                         res = ("%d is a prime number" %num)
     else:
         res = ("%d is not a prime number" %num)
+    
+    # Send back result of algorithm
     s.sendto(res.encode(), addr)
